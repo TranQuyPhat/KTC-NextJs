7bcbc0a960623ff564f78fe45644a0b5680bc5f3
q_phat <Quyphat.162@gmail.com>
2025-07-14 23:55:54 +0700

    first-v2

--- src/app/day11/hw/pages/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
-      <p className="mt-2">The page you’re looking for does not exist.</p>
-    </div>
-  );
-}
--- src/app/day11/hw/pages/block.tsx
+++
@@ -1,3 +0,0 @@
-export default function Blog() {
-  return <h1 className="text-2xl font-bold">Blog Page</h1>;
-}
